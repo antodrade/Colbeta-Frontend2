277bf1e93a0e3b0d760b46d7361c866b51c795c6
antodrade <antoandrade1988@gmail.com>
2026-02-17 15:19:20 -0500

    anexadas variables de environment

--- src/app/empresa.service.ts
+++ src/app/empresa.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Empresa } from './models/empresa';
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,8 @@ import { Empresa } from './models/empresa';
 export class EmpresaService {
 
 //private urlBase = "http://localhost:8080/hole2/empresas"
-private urlBase = "https://colbeta-backend.onrender.com/hole2/empresas"
+//private urlBase = "https://colbeta-backend.onrender.com/hole2/empresas"
+private urlBase = `${environment.apiUrl}/hole2/empresas`; 
 
 
   constructor(private clienteHttp: HttpClient ) { }
--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Usuario } from './models/usuario';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -10,7 +11,8 @@ import { Usuario } from './models/usuario';
 export class UsuarioService {
 
  //private urlBase = "http://localhost:8080/hole/usuarios"
- private urlBase = "https://colbeta-backend.onrender.com/hole/usuarios"
+ //private urlBase = "https://colbeta-backend.onrender.com/hole/usuarios"
+ private urlBase = `${environment.apiUrl}/hole/usuarios`;
 
 
   constructor(private clienteHttp: HttpClient) { }
